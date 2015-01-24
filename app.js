6a93f81fdc1b4054ebe2726e618e6cb04c969a56
Ross Mackay <ross@mcky.me>
2015-01-24 07:55:01 +0000

    Fix #5 for now. Fix #13.

--- app.js
+++ app.js
@@ -1,84 +1,69 @@
-window.onload = function() {
-	var canvas = document.getElementById('canvas')
-		,context = canvas.getContext('2d')
-		,download = document.getElementById('download')
-		,textElem = document.getElementById('text')
-		,nameElem = document.getElementById('name')
-		,twitterElem = document.getElementById('twitter')
-		,textFields = [textElem, nameElem, twitterElem]
-		,quote = document.getElementById('quote')
-		,overlay = document.getElementById('overlay')
-		,checkboxes = [quote, overlay]
-		,text = textElem.value
-		,name = nameElem.value
-		,twitterHandle = twitterElem.value
-		,textColor = '#FFF'
-		,img
+var canvas = document.getElementById('canvas')
+	,context = canvas.getContext('2d')
+	,download = document.getElementById('download')
+	,uploadElem = document.getElementById('upload')
+	,textElem = document.getElementById('text')
+	,nameElem = document.getElementById('name')
+	,twitterElem = document.getElementById('twitter')
+	,textFields = document.getElementsByClassName('textfield')
+	,quote = document.getElementById('quote')
+	,overlay = document.getElementById('overlay')
+	,checkboxes = document.getElementsByClassName('checkbox')
+	,text = textElem.value
+	,name = nameElem.value
+	,twitterHandle = twitterElem.value
+	,textColor = '#FFF'
+	,img
 
+var backgroundImg = new Image()
 
-	var backgroundImg = new Image();
-	backgroundImg.src = './feature.jpg';
-	// backgroundImg.src = './card.png';
+var LSLogo = new Image()
+LSLogo.src = './LSLogo.png'
 
-	var LSLogo = new Image();
-	LSLogo.src = './LSLogo.png';
+var drawToCanvas = function(){
+	context.clearRect (0, 0, canvas.width, canvas.height);
+	context.drawImage(backgroundImg, 0, 0)
 
-	var drawToCanvas = function(){
-		context.drawImage(backgroundImg, 0, 0);
-
-		// Overlay
-		if (overlay.checked) {
-			context.fillStyle = "rgba(0,0,0,0.5)";
-			context.fillRect(0,0,canvas.width,canvas.height)
-		}
-
-		// Logo
-		var logoH = 200/4
-			,logoW = 1075/4
-			,logoOffsetH = canvas.height - logoH - 25
-			,logoOffsetW = canvas.width - logoW - 25
-		context.drawImage(LSLogo, logoOffsetW, logoOffsetH, logoW, logoH);
-
-		context.fillStyle = textColor
-
-		// Quote marks
-		if (quote.checked) {
-			text = textElem.value + ' ”'
-			context.font = "150px Droid Serif";
-			context.fillText('“', 25, 150)
-		} else {
-			text = textElem.value
-		}
-
-		// Main text
-		context.font = "26px Droid Serif";
-		wrapText(context, text, 103, 98, 400, 33);
-
-
-		// Name
-		context.font = "bold 24px Helvetica";
-		context.fillText(nameElem.value, 103, 235)
-
-		// Twitter
-		context.fillStyle = "#d2232a";
-		context.font = "bold 18px Helvetica";
-		context.fillText('@'+twitterElem.value, 103, 258)
-
-
-		img = canvas.toDataURL('image/png');
-		download.href = img
+	// Overlay
+	if (overlay.checked) {
+		context.fillStyle = "rgba(0,0,0,0.5)"
+		context.fillRect(0,0,canvas.width,canvas.height)
 	}
 
-	for (var i = 0; i < textFields.length; i++) {
-		textFields[i].addEventListener('keyup', drawToCanvas)
-	}
-	for (var i = 0; i < checkboxes.length; i++) {
-		checkboxes[i].addEventListener('change', drawToCanvas)
-	}
-	backgroundImg.onload = drawToCanvas
+	// Logo
+	var logoH = 200/4
+		,logoW = 1075/4
+		,logoOffsetH = canvas.height - logoH - 25
+		,logoOffsetW = canvas.width - logoW - 25
+	context.drawImage(LSLogo, logoOffsetW, logoOffsetH, logoW, logoH)
 
-	// Google font loading
-	this.setTimeout(drawToCanvas, 200)
+	context.fillStyle = textColor
+
+	// Quote marks
+	if (quote.checked) {
+		text = textElem.value + ' ”'
+		context.font = "150px Droid Serif"
+		context.fillText('“', 25, 150)
+	} else {
+		text = textElem.value
+	}
+
+	// Main text
+	context.font = "26px Droid Serif"
+	wrapText(context, text, 103, 98, 400, 33)
+
+
+	// Name
+	context.font = "bold 24px Helvetica"
+	context.fillText(nameElem.value, 103, 235)
+
+	// Twitter
+	context.fillStyle = "#d2232a"
+	context.font = "bold 18px Helvetica"
+	context.fillText('@'+twitterElem.value, 103, 258)
+
+	img = canvas.toDataURL('image/png')
+	download.href = img
 }
 
 // From http://www.html5canvastutorials.com/tutorials/html5-canvas-wrap-text-tutorial/
@@ -103,5 +88,50 @@ var wrapText = function(context, text, x, y, maxWidth, lineHeight) {
 	// Check last character isn't a quote
 	// return bottom bound?
 }
+
+
+var fileDraggedOver = function(e) {
+	e.stopPropagation()
+	e.preventDefault()
+	e.dataTransfer.dropEffect = 'copy'
+	upload.classList.add('upload--active')
+}
+
+var fileDragLeave = function(e) {
+	upload.classList.remove('upload--active')
+}
+
+var fileDropped = function(e) {
+	e.stopPropagation()
+	e.preventDefault()
+
+	var files = e.dataTransfer.files
+		,f = files[0]
+		,reader = new FileReader()
+
+	reader.onload = (function(theFile) {
+		return function(e) {
+			backgroundImg.src = e.target.result
+			drawToCanvas()
+		}
+	})(f)
+	reader.readAsDataURL(f)
+
+	upload.classList.remove('upload--active')
+}
+
+for (var i = 0; i < textFields.length; i++) {
+	textFields[i].addEventListener('keyup', drawToCanvas)
+}
+for (var i = 0; i < checkboxes.length; i++) {
+	checkboxes[i].addEventListener('change', drawToCanvas)
+}
+
+uploadElem.addEventListener('dragover', fileDraggedOver, false)
+uploadElem.addEventListener('drop', fileDropped, false)
+uploadElem.addEventListener('dragleave', fileDragLeave, false)
+
+// Google font loading
+this.setTimeout(drawToCanvas, 200)
 
 
